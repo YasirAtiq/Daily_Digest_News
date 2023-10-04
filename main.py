96a6d1f4557c241573cfb56b1d9af3c3665bcde0
Yasir <DPSA00609@mypaceschool.com>
2023-10-04 18:11:16 +0400

    Five improvements

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 import requests
 from send_email import send_mail
 
-api_key = "7cb08aeed3fc44feb476188870547c64"
-url = (f"https://newsapi.org/v2/everything?"
-       f"q=tesla&from=2023-09-04&sortBy=publishedAt&apiKey={api_key}")
+## Adding which topic to receive news on
+topic = "tesla"
 
+## Adding url
+api_key = "7cb08aeed3fc44feb476188870547c64"
+url = ("https://newsapi.org/v2/everything?"
+       f"q={topic}&from=2023-09-04&sortBy=publishedAt&"
+       f"apiKey={api_key}&"
+       "language=en")
+## Getting API
 request = requests.get(url)
 content = request.json()
 
+## Sending email
 email_body = ""
-for article in content["articles"]:
-    email_body = email_body + f"{article['title']} \n" + article["description"] + 2*"\n"
+for article in content["articles"][:20]:
+    if article["title"] is not None:
+        email_body = (email_body + article['title'] + "\n" + article["description"]
+                      + "\n" + article['url'] + 2*"\n")
 
 send_mail("Today's Tesla News", message=email_body)
